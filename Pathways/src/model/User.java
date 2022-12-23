1cb024fa6cc6305711c1b525642bd45309b40aea
Salvatoree12 <120659943+Salvatoree12@users.noreply.github.com>
2022-12-23 15:02:31 +0100

    Classi Model
    Classi User, classe nodo, classe AttrazioneNodo

--- Pathways/src/model/User.java
+++ Pathways/src/model/User.java
@@ -2,4 +2,71 @@ package model;
 
 public class User {
 
+	private String nome;
+	private String cognome;
+	private String password;
+	private String email;
+	private int id;
+	
+	public User(String nome, String cognome, String passoword, String email) {
+		
+		this.nome = nome;
+		this.cognome = cognome;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public User(String nome, String cognome, String passoword, String email,int id) {
+		
+		this.nome = nome;
+		this.cognome = cognome;
+		this.password = password;
+		this.email = email;
+		this.id = id;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [nome=" + nome + ", cognome=" + cognome + ", password=" + password + ", email=" + email + ", id="
+				+ id + "]";
+	}
+	
+	
+	
+	
+	
 }
