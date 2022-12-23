4f01a971ad71cf404c7f619e9092747359e1ba9a
Davide453 <111684228+Davide453@users.noreply.github.com>
2022-12-23 14:55:10 +0100

    commitWebContent

---
+++ Pathways/WebContent/js/mappa.js
@@ -0,0 +1,143 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let map, infoWindow;
+
+function initMap() {
+  // The location of start
+
+  const start = { lat: 45.477781673393366, lng: 9.186916132921075 };
+  // The map, centered at start
+  map = new google.maps.Map(document.getElementById("map"), {
+    mapTypeControl: false,
+    zoom: 12,
+    center: start,
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_CENTER,
+    },
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP,
+    },
+    mapId: "ecdb3dce61875a18",
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Trova la tua posizione";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Posizione trovata.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+  let marker;
+
+  function useMarkerData(callback) {
+    if (marker) {
+      // Se markerData è già stato assegnato, eseguiamo la callback
+      callback(marker);
+    } else {
+      // Altrimenti, impostiamo un timer per controllare periodicamente se markerData è stato assegnato
+      const intervalId = setInterval(() => {
+        if (marker) {
+          // Se markerData è stato assegnato, eseguiamo la callback e cancelliamo il timer
+          callback(marker);
+          clearInterval(intervalId);
+        }
+      }, 100);
+    }
+  }
+
+  // Eseguiamo il fetch
+  fetch('./json/Attrazioni.json')
+    .then((response) => response.json())
+    .then((json) => {
+      marker = json.attrazioni;
+      console.log(marker);
+      for (let i = 0; i < marker.length; i++) {
+        console.log(marker[i]);
+        let markerAttrazione = new google.maps.Marker({
+          position: { lat: marker[i].latitudine, lng: marker[i].longitudine },
+          map,
+          title: marker[i].nome,
+        });
+
+        const posizione = { lat: marker[i].latitudine, lng: marker[i].longitudine };
+
+        let nome = marker[i].nome;
+
+        let text = `${nome}`;
+
+        const contentString =
+          '<div id="content">' +
+          '<div id="siteNotice">' +
+          "</div>" +
+          '<h1 id="firstHeading" class="firstHeading">'+ text + '</h1>' +
+          '<div id="bodyContent">' +
+          '<p>lorem ipsum dolores </p>' +
+          '<button type="button" class="btn btn-primary">Aggiungi al tuo percorso</button>'
+          "</div>" +
+          "</div>";
+        const infowindow = new google.maps.InfoWindow({
+          content: contentString,
+          ariaLabel: marker[i].nome,
+        });
+
+        markerAttrazione.addListener("click", () => {
+          infowindow.open({
+            position: posizione,
+            anchor: markerAttrazione,
+            map,
+          });
+        });
+      }
+
+
+    });
+
+  // Usiamo useMarkerData per accedere a markerData in modo sicuro
+  useMarkerData((data) => {
+    console.log(data);
+  });
+
+
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Errore: Il servizio di geolocazione è fallito."
+      : "Errore: Il tuo browser non supporta la geolocalizzazione."
+  );
+  infoWindow.open(map);
+}
+
+
+window.initMap = initMap;
