253be5f01ae5fe8f15fd5f690f31927aa29d2638
Davide453 <111684228+Davide453@users.noreply.github.com>
2023-01-03 18:29:35 +0100

    ottenuto valori da JSON

--- Pathways/src/control/Percorso.java
+++ Pathways/src/control/Percorso.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,6 +16,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.AttrazioneNodo;
 
@@ -44,9 +49,41 @@ public class Percorso extends HttpServlet {
 		String line;
 		while ((line = reader.readLine()) != null) {
 			sb.append(line);
+
 		}
-		
-		System.out.println(sb);
+
+		Gson g = new Gson();
+		System.out.println("g.tojson(sb)" + g.toJson(sb));
+
+		String test = g.toJson(sb);
+		System.out.println(test);
+
+		JsonArray data = (JsonArray) new JsonParser().parse(sb.toString());
+
+		System.out.println(data);
+
+		System.out.println(data.get(0));
+		// JsonObject jsonObject = data.get(0);
+
+		for (int i = 0; i < data.size(); i++) {
+			JsonObject jsonObject = data.get(i).getAsJsonObject();
+
+			JsonElement nameElement = jsonObject.get("name");
+
+			JsonObject geometryObject = (JsonObject) jsonObject.get("geometry");
+
+			JsonElement geometryElement = geometryObject.get("location");
+
+			JsonElement namePlaceId = jsonObject.get("place_id");
+
+			JsonElement vicinityElement = jsonObject.get("vicinity");
+
+			System.out.println(nameElement);
+			System.out.println(geometryElement);
+			System.out.println(namePlaceId);
+			System.out.println(vicinityElement);
+			
+		}
 
 		response.setCharacterEncoding("UTF-8");
 
