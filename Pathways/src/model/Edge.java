2aa880e2ec48d7ec0a6abfc8a3f342815d85f1e3
Davide453 <111684228+Davide453@users.noreply.github.com>
2023-01-04 15:35:58 +0100

    Implementazione Dijkstra

---
+++ Pathways/src/model/Edge.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Edge {
+
+	private AttrazioneNodo origine;
+	private AttrazioneNodo destinazione;
+	private double peso;
+	
+	
+	
+	public Edge(AttrazioneNodo origine, AttrazioneNodo destinazione, double peso) {
+		this.origine = origine;
+		this.destinazione = destinazione;
+		this.peso = peso;
+	}
+	public AttrazioneNodo getOrigine() {
+		return origine;
+	}
+	public void setOrigine(AttrazioneNodo origine) {
+		this.origine = origine;
+	}
+	public AttrazioneNodo getDestinazione() {
+		return destinazione;
+	}
+	public void setDestinazione(AttrazioneNodo destinazione) {
+		this.destinazione = destinazione;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	@Override
+	public String toString() {
+		return "Edge origine=" + origine + " destinazione=" + destinazione + " peso=" + peso + " \n";
+	}
+	
+	
+}
